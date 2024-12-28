2ef7d5991064634984d36e601880535cb91f4277
Jorge Timón <jtimon@jtimon.cc>
2024-12-28 03:41:16 +0100

    save

---
+++ src/main.rs
@@ -0,0 +1,575 @@
+use std::env;
+use std::io;
+use std::io::Write; // <--- bring flush() into scope
+use std::fs;
+use std::io::ErrorKind;
+
+#[derive(Debug, Clone)]
+enum TokenType {
+    // basic
+    Eof,
+
+    // Single-character tokens.
+    Minus, Plus, Slash, Star,
+    LeftParen, RightParen, LeftBrace, RightBrace,
+    Comma, Dot, Colon, Semicolon,
+
+    // One or two character tokens.
+    Not, NotEqual,
+    Equal, EqualEqual,
+    Greater, GreaterEqual,
+    Lesser, LesserEqual,
+
+    // Literals.
+    Identifier, String, Number,
+
+    // Reserved words.
+    True, False, And, Or,
+    Const, Var,
+    Struct, Enum,
+    Fn, Proc,
+    Match, If, Else, While, For, In,
+    Return, Returns, Throw, Rethrow, Throws, Catch,
+    Debug,
+
+    // Errors
+    Invalid,
+    UnterminatedString,
+}
+
+#[derive(Debug, Clone)]
+struct Token {
+    token_type: TokenType,
+    start: usize,
+    end: usize,
+    line: usize,
+}
+
+fn token_type_to_string(token_type: &TokenType) -> String {
+    match token_type {
+        TokenType::Eof => "Eof".to_string(),
+        TokenType::Minus => "Minus".to_string(),
+        TokenType::Plus => "Plus".to_string(),
+        TokenType::Slash => "Slash".to_string(),
+        TokenType::Star => "Star".to_string(),
+        TokenType::LeftParen => "LeftParen".to_string(),
+        TokenType::RightParen => "RightParen".to_string(),
+        TokenType::LeftBrace => "LeftBrace".to_string(),
+        TokenType::RightBrace => "RightBrace".to_string(),
+        TokenType::Comma => "Comma".to_string(),
+        TokenType::Dot => "Dot".to_string(),
+        TokenType::Colon => "Colon".to_string(),
+        TokenType::Semicolon => "Semicolon".to_string(),
+        TokenType::Not => "Not".to_string(),
+        TokenType::NotEqual => "NotEqual".to_string(),
+        TokenType::Equal => "Equal".to_string(),
+        TokenType::EqualEqual => "EqualEqual".to_string(),
+        TokenType::Greater => "Greater".to_string(),
+        TokenType::GreaterEqual => "GreaterEqual".to_string(),
+        TokenType::Lesser => "Lesser".to_string(),
+        TokenType::LesserEqual => "LesserEqual".to_string(),
+        TokenType::Identifier => "Identifier".to_string(),
+        TokenType::String => "String".to_string(),
+        TokenType::Number => "Number".to_string(),
+        TokenType::True => "True".to_string(),
+        TokenType::False => "False".to_string(),
+        TokenType::And => "And".to_string(),
+        TokenType::Or => "Or".to_string(),
+        TokenType::Const => "Const".to_string(),
+        TokenType::Var => "Var".to_string(),
+        TokenType::Fn => "Fn".to_string(),
+        TokenType::Proc => "Proc".to_string(),
+        TokenType::Struct => "Struct".to_string(),
+        TokenType::Enum => "Enum".to_string(),
+        TokenType::Match => "Match".to_string(),
+        TokenType::If => "If".to_string(),
+        TokenType::Else => "Else".to_string(),
+        TokenType::While => "While".to_string(),
+        TokenType::For => "For".to_string(),
+        TokenType::In => "In".to_string(),
+        TokenType::Return => "Return".to_string(),
+        TokenType::Returns => "Returns".to_string(),
+        TokenType::Throw => "Throw".to_string(),
+        TokenType::Rethrow => "Rethrow".to_string(),
+        TokenType::Throws => "Throws".to_string(),
+        TokenType::Catch => "Catch".to_string(),
+        TokenType::Debug => "Debug".to_string(),
+        TokenType::Invalid => "Invalid".to_string(),
+        TokenType::UnterminatedString => "UnterminatedString".to_string(),
+        // TokenType:: => "".to_string(),
+        // _ => "Unknown".to_string(),
+    }
+}
+
+fn is_digit(source: &String, pos: usize) -> bool {
+    match &source[pos..pos+1] {
+        "0" => true,
+        "1" => true,
+        "2" => true,
+        "3" => true,
+        "4" => true,
+        "5" => true,
+        "6" => true,
+        "7" => true,
+        "8" => true,
+        "9" => true,
+        _ => false,
+    }
+}
+
+fn is_alphanumeric(source: &String, pos: usize) -> bool {
+    // let ch = &source[pos..pos+1].chars().next().unwrap();
+    // ch.is_ascii_alphanumeric()
+
+
+    source[pos..pos+1].bytes().all(|b| matches!(b, b'a'..=b'z'))
+        || source[pos..pos+1].bytes().all(|b| matches!(b, b'A'..=b'Z'))
+}
+
+fn get_identifier_type(identifier: &str) -> TokenType {
+    // println!("Identifier literal {}", identifier);
+    match identifier {
+        "true" => TokenType::True,
+        "false" => TokenType::False,
+        "and" => TokenType::And,
+        "or" => TokenType::Or,
+        "const" => TokenType::Const,
+        "var" => TokenType::Var,
+        "struct" => TokenType::Struct,
+        "enum" => TokenType::Enum,
+        "fn" => TokenType::Fn,
+        "proc" => TokenType::Proc,
+        "match" => TokenType::Match,
+        "if" => TokenType::If,
+        "else" => TokenType::Else,
+        "while" => TokenType::While,
+        "for" => TokenType::For,
+        "in" => TokenType::In,
+        "return" => TokenType::Return,
+        "returns" => TokenType::Returns,
+        "throw" => TokenType::Throw,
+        "rethrow" => TokenType::Rethrow,
+        "throws" => TokenType::Throws,
+        "catch" => TokenType::Catch,
+        "debug" => TokenType::Debug,
+        _ => TokenType::Identifier,
+    }
+}
+
+fn scan_tokens(source: &String) -> Vec<Token> {
+    let mut tokens : Vec<Token> = Vec::new();
+
+    let eof_pos : usize = source.len();
+    let mut pos = 0;
+    let mut line = 0;
+
+    while pos < eof_pos {
+        let start = pos;
+
+        if is_digit(source, pos) {
+            while pos < eof_pos && is_digit(source, pos) {
+                pos = pos + 1;
+            }
+            // Look for a fractional part.
+            if &source[pos..pos+1] == "." && is_digit(source, pos+1) {
+                pos = pos + 1;
+                while pos < eof_pos && is_digit(source, pos) {
+                    pos = pos + 1;
+                }
+            }
+            tokens.push(Token { token_type: TokenType::Number, start: start, end: pos + 1, line: line });
+        } else {
+
+            let token_type = match &source[pos..pos+1] {
+                " " => { pos = pos + 1; continue; },
+                "\r" => { pos = pos + 1; continue; },
+                "\t" => { pos = pos + 1; continue; },
+                "\n" => { pos = pos + 1; line = line + 1; continue; },
+                "(" => TokenType::LeftParen,
+                ")" => TokenType::RightParen,
+                "{" => TokenType::LeftBrace,
+                "}" => TokenType::RightBrace,
+                "-" => TokenType::Minus,
+                "+" => TokenType::Plus,
+                "*" => TokenType::Star,
+                "," => TokenType::Comma,
+                "." => TokenType::Dot,
+                ":" => TokenType::Colon,
+                ";" => TokenType::Semicolon,
+                "=" => if &source[pos+1..pos+2] == "=" { pos = pos + 1; TokenType::EqualEqual } else { TokenType::Equal },
+                "<" => if &source[pos+1..pos+2] == "=" { pos = pos + 1; TokenType::LesserEqual } else { TokenType::Lesser },
+                ">" => if &source[pos+1..pos+2] == "=" { pos = pos + 1; TokenType::GreaterEqual } else { TokenType::Greater },
+                "!" => if &source[pos+1..pos+2] == "=" { pos = pos + 1; TokenType::NotEqual } else { TokenType::Not },
+
+                "/" => match &source[pos+1..pos+2] {
+                    "/" => {
+                        pos = pos + 1;
+                        while pos + 1 < eof_pos && &source[pos..pos+1] != "\n" {
+                            pos = pos + 1;
+                        }
+                        continue;
+                        // TODO allow the other type of commments, allowing nesting
+                    },
+                    _ => TokenType::Slash,
+                },
+
+                "\"" => {
+                    pos = pos + 1;
+                    while pos + 1 < eof_pos && &source[pos..pos+1] != "\"" {
+                        pos = pos + 1;
+                    }
+                    // pos = pos - 1;
+                    match &source[pos..pos+1] {
+                        "\"" => TokenType::String,
+                        _ => TokenType::UnterminatedString,
+                    }
+                },
+
+                _ => {
+                    if is_alphanumeric(source, pos){
+                        pos = pos + 1;
+                        // FIX invalid characters
+                        while pos < eof_pos && (is_alphanumeric(source, pos) || is_digit(source, pos)) {
+                            pos = pos + 1;
+                        }
+                        pos = pos - 1;
+                        get_identifier_type(&source[start..pos+1])
+
+                    } else {
+                        TokenType::Invalid
+                    }
+                },
+            }; // let match
+            // println!("Current token line {}, start: {}, end {}, identifier type: {} identifier string: __{}__",
+                     // line, start, pos+1, token_type_to_string(&token_type), &source[start..pos+1]);
+            tokens.push(Token { token_type: token_type, start: start, end: pos + 1, line: line });
+            pos = pos + 1;
+        } // else
+    } // while
+    tokens.push(Token { token_type: TokenType::Eof, start: pos, end: pos + 1, line: line });
+    tokens
+}
+
+// fn unary(tokens: &Vec<Token>, current: usize) -> Expr {
+
+//     let t: Token = tokens.get(current).unwrap().clone();
+//     let is_unary = match &t.token_type {
+//         TokenType::Minus => true,
+//         TokenType::Not => true,
+//         _ => false,
+//     };
+
+//     if is_unary {
+//         let mut params : Vec<Expr> = Vec::new();
+//         params.push(unary(&tokens, current + 1));
+//         Expr { operator: t, params: params}
+//     } else {
+//         primary(&tokens, current)
+//     }
+// }
+
+// fn factor(tokens: &Vec<Token>, current: usize) -> Expr {
+//     let expr: Expr = unary(&tokens, current);
+
+//     let t: Token = tokens.get(current).unwrap().clone();
+//     if match &t.token_type {
+//         TokenType::Slash => true,
+//         TokenType::Star => true,
+//         _ => false,
+//     } {
+//         let t: Token = tokens.get(current).unwrap().clone();
+//         let mut params : Vec<Expr> = Vec::new();
+//         params.push(expr);
+//         params.push(unary(&tokens, current + 1));
+//         Expr { operator: t, params: params}
+//     } else {
+//         expr
+//     }
+// }
+
+// fn term(tokens: &Vec<Token>, current: usize) -> Expr {
+//     let expr: Expr = factor(&tokens, current);
+
+//     let t: Token = tokens.get(current).unwrap().clone();
+//     if match &t.token_type {
+//         TokenType::Minus => true,
+//         TokenType::Plus => true,
+//         _ => false,
+//     } {
+//         let t: Token = tokens.get(current).unwrap().clone();
+//         let mut params : Vec<Expr> = Vec::new();
+//         params.push(expr);
+//         params.push(factor(&tokens, current + 1));
+//         Expr { operator: t, params: params}
+//     } else {
+//         expr
+//     }
+// }
+
+// fn is_comparison(t: &Token) -> bool {
+//     match &t.token_type {
+//         TokenType::Greater => true,
+//         TokenType::GreaterEqual => true,
+//         TokenType::Lesser => true,
+//         TokenType::LesserEqual => true,
+//         _ => false,
+//     }
+// }
+
+// fn comparison(tokens: &Vec<Token>, current: usize) -> Expr {
+//     let expr: Expr = term(&tokens, current);
+
+//     let t: Token = tokens.get(current).unwrap().clone();
+//     if is_comparison(&t) {
+//         let t: Token = tokens.get(current).unwrap().clone();
+//         let mut params : Vec<Expr> = Vec::new();
+//         params.push(expr);
+//         params.push(term(&tokens, current + 1));
+//         Expr { operator: t, params: params}
+//     } else {
+//         expr
+//     }
+// }
+
+// fn equality(tokens: &Vec<Token>, current: usize) -> Expr {
+//     let expr: Expr = comparison(&tokens, current);
+
+//     let t: Token = tokens.get(current).unwrap().clone();
+//     let is_equality = match &t.token_type {
+//         TokenType::NotEqual => true,
+//         TokenType::EqualEqual => true,
+//         _ => false,
+//    };
+//    if is_equality {
+//         let t: Token = tokens.get(current).unwrap().clone();
+//         let mut params : Vec<Expr> = Vec::new();
+//         params.push(expr);
+//         params.push(comparison(&tokens, current + 1));
+//         Expr { operator: t, params: params}
+//    } else {
+//        expr
+//    }
+// }
+
+// fn expression(tokens: &Vec<Token>, current: usize) -> Expr {
+//     let expr: Expr = equality(&tokens, current);
+//     expr
+// }
+
+// fn is_literal_boolean(tokens: &Vec<Token>, current: usize) -> bool {
+//     let t = tokens.get(current).unwrap();
+//     match t.token_type {
+//         TokenType::True => true,
+//         TokenType::False => true,
+//         _  => false,
+//     }
+// }
+
+// fn boolean(tokens: &Vec<Token>, current: usize) -> Expr {
+//     let t: Token = tokens.get(current).unwrap().clone();
+//     let mut params : Vec<Expr> = Vec::new();
+
+//     match t.token_type {
+//         TokenType::True => Expr { operator: t, params: params},
+//         TokenType::False => Expr { operator: t, params: params},
+//         _  => false,
+//     }
+// }
+
+
+#[derive(Clone)]
+struct Expr {
+    token_index: usize,
+    params: Vec<Expr>,
+}
+
+fn to_ast_str(tokens: &Vec<Token>, e: &Expr) -> String {
+    let mut ast_str = "".to_string();
+
+    let t = tokens.get(e.token_index).unwrap();
+
+    if e.params.len() > 0 {
+        ast_str.push_str("(");
+    }
+
+    ast_str.push_str(&format!("{}", token_type_to_string(&t.token_type)));
+
+    if e.params.len() > 0 {
+        for se in e.params.iter() {
+            ast_str.push_str(&format!(" {}", to_ast_str(&tokens, &se)));
+        }
+
+        ast_str.push_str(")");
+    }
+    ast_str
+}
+
+fn literal(current: usize) -> Expr {
+    let params : Vec<Expr> = Vec::new();
+    Expr { token_index: current, params: params}
+}
+
+fn is_eof(tokens: &Vec<Token>, current: usize) -> bool {
+    current > tokens.len() || match tokens.get(current).unwrap().token_type {
+        TokenType::Eof => true,
+        _ => false,
+    }
+}
+
+fn primary(tokens: &Vec<Token>, current: usize) -> Expr {
+
+    // println!("primary debug: {}", current);
+    let t = tokens.get(current).unwrap();
+    match &t.token_type {
+        TokenType::String => literal(current),
+        TokenType::Identifier => literal(current),
+        TokenType::Number => literal(current),
+        TokenType::True => literal(current),
+        TokenType::False => literal(current),
+        TokenType::LeftParen => {
+            let mut rightparent_found = false;
+            let mut params : Vec<Expr> = Vec::new();
+            let mut end_list = current + 1;
+            let mut list_t = tokens.get(end_list).unwrap();
+            // println!("primary debug LeftParen: {} {}", current, end_list);
+            while !(rightparent_found || is_eof(&tokens, end_list)) {
+                // println!("primary debug LeftParen while: {} {}", current, end_list);
+                match list_t.token_type {
+                    TokenType::RightParen => {
+                        rightparent_found = true;
+                    },
+                    _ => {
+                        params.push(primary(&tokens, end_list));
+                        end_list = end_list + 1;
+                        list_t = tokens.get(end_list).unwrap();
+                    },
+                }
+            }
+            match list_t.token_type {
+                TokenType::RightParen => Expr { token_index: current, params: params},
+                _ => panic!("compiler error (line {}): Expected closing parentheses.", t.line),
+            }
+        }
+        _ => panic!("compiler error (line {}): Expected primary expression, found {:?}.", t.line, t.token_type),
+    }
+}
+
+fn parse_tokens(tokens: &Vec<Token>) -> String {
+    let current: usize = 0;
+
+    let e: Expr = primary(tokens, current);
+    println!("Total tokens parsed: {}", current);
+
+    to_ast_str(&tokens, &e)
+}
+
+// struct ParseError {
+//     error_msg = String,
+//     line: usize,
+// }
+
+// fn print_parse_error(error: ParseError) {
+// }
+
+fn print_lex_error(source: &String, t: &Token, num_error: usize, msg: &str) {
+    let max_symbol_len = 20;
+    let mut end_symbol = t.end;
+    if end_symbol - t.start > max_symbol_len {
+        end_symbol = max_symbol_len;
+    }
+    println!("Lexical error {} line {}: {}. Offending symbol: {}", num_error, t.line, msg, &source[t.start..end_symbol]);
+}
+
+fn run(source: &String) -> String {
+    let tokens: Vec<Token> = scan_tokens(&source);
+    if tokens.len() < 1 {
+        panic!("compiler error (line {}): End of file not found.", 0);
+    } else if is_eof(&tokens, 0) {
+        panic!("compiler error (line {}): Nothing to be done", tokens.get(0).unwrap().line);
+    }
+
+    let mut errors_found : usize = 0;
+    for t in &tokens {
+        match t.token_type {
+            TokenType::Invalid => {
+                print_lex_error(&source, &t, errors_found, "Invalid character");
+                errors_found = errors_found + 1;
+            },
+            TokenType::UnterminatedString => {
+                print_lex_error(&source, &t, errors_found, "Unterminated String");
+                errors_found = errors_found + 1;
+            },
+            _ => {},
+        }
+    }
+    if errors_found > 0 {
+        panic!("Compiler errors: {} lexical errors found", errors_found);
+    }
+
+    parse_tokens(&tokens)
+}
+
+// struct TestData {
+//     expected: String,
+//     found: String,
+// }
+
+// fn t(a: &str, b: &str) -> TestData {
+//     TestData {expected: a.to_string(), found: b.to_string()}
+// }
+
+// fn test() {
+//     let to_test = [
+//         t("aaa", "aaa"),
+//         t(run(&"aaa".to_string()).as_str(), "bbb"),
+//     ];
+//     for e in to_test {
+//         if e.expected != e.found {
+//             println!("test ERROR expected {} found {}", e.expected, e.found);
+//         }
+//     }
+// }
+
+fn run_file(path: &String) {
+    let source: String = match fs::read_to_string(path) {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => {
+                panic!("File {} not found.", path);
+            },
+            other_error => {
+                panic!("Problem opening the file: {other_error:?}");
+            },
+        },
+    };
+    println!("AST: {}", run(&source));
+}
+
+fn run_prompt() {
+    loop {
+        print!("cil> ");
+        io::stdout().flush().unwrap();
+
+        let mut line = String::new();
+        io::stdin()
+            .read_line(&mut line)
+            .expect("Failed to read line");
+
+        if line.len() == 1 { break; }
+
+        println!("{}", run(&line));
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // test();
+    if args.len() > 2 {
+        println!("Usage: cargo run [pathToScript]");
+    } else if args.len() > 1 {
+        run_file(&args[1]);
+    } else {
+        run_prompt();
+    }
+}
