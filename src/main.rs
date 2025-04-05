7c4b0d2019ba5991b0c6be68cf10115201668f44
Jorge Timón <jtimon@jtimon.cc>
2025-04-05 16:13:33 +0200

    core funcs: introduce str_len and str_get_substr

--- src/main.rs
+++ src/main.rs
@@ -1496,6 +1496,8 @@ fn is_core_func(proc_name: &str) -> bool {
         "atoi" => true,
         "str_eq" => true,
         "concat" => true,
+        "str_len" => true,
+        "str_get_substr" => true,
         _ => false,
     }
 }
@@ -2428,14 +2430,28 @@ fn eval_core_func_str_eq(mut context: &mut Context, e: &Expr) -> String {
     assert!(e.params.len() == 3, "{} Error: Core func 'str_eq' takes exactly 2 arguments. This should never happen.", LANG_NAME);
     let a = &eval_expr(&mut context, e.get(1));
     let b = &eval_expr(&mut context, e.get(2));
-    (a == b).to_string()
+    return (a == b).to_string()
 }
 
 fn eval_core_func_concat(mut context: &mut Context, e: &Expr) -> String {
     assert!(e.params.len() == 3, "{} Error: Core func 'concat' takes exactly 2 arguments. This should never happen.", LANG_NAME);
     let a = eval_expr(&mut context, e.get(1));
     let b = eval_expr(&mut context, e.get(2));
-    format!("{}{}", a, b)
+    return format!("{}{}", a, b)
+}
+
+fn eval_core_func_str_len(mut context: &mut Context, e: &Expr) -> String {
+    assert!(e.params.len() == 2, "{} Error: Core func 'concat' takes exactly 1 argument. This should never happen.", LANG_NAME);
+    let a = eval_expr(&mut context, e.get(1));
+    return format!("{}", a.len())
+}
+
+fn eval_core_func_str_get_substr(mut context: &mut Context, e: &Expr) -> String {
+    assert!(e.params.len() == 4, "{} Error: Core func 'concat' takes exactly 3 arguments. This should never happen.", LANG_NAME);
+    let a = eval_expr(&mut context, e.get(1));
+    let start = &eval_expr(&mut context, e.get(2)).parse::<i64>().unwrap();
+    let end = &eval_expr(&mut context, e.get(3)).parse::<i64>().unwrap();
+    return format!("{}", &a[*start as usize..*end as usize]);
 }
 
 fn eval_core_func_lt(mut context: &mut Context, e: &Expr) -> String {
@@ -2673,6 +2689,8 @@ fn eval_core_func_call(name: &str, mut context: &mut Context, e: &Expr) -> Strin
         "eq" => eval_core_func_eq(&mut context, &e),
         "str_eq" => eval_core_func_str_eq(&mut context, &e),
         "concat" => eval_core_func_concat(&mut context, &e),
+        "str_len" => eval_core_func_str_len(&mut context, &e),
+        "str_get_substr" => eval_core_func_str_get_substr(&mut context, &e),
         "lt" => eval_core_func_lt(&mut context, &e),
         "lteq" => eval_core_func_lteq(&mut context, &e),
         "gt" => eval_core_func_gt(&mut context, &e),
