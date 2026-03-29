e059090baadff165a9274652c82be284292b8d1e
Jorge Timón <jtimon@jtimon.cc>
2026-03-29 22:32:58 +0100

    Typer: translate resolve_fcall_return_type

--- boot/modes.h
+++ boot/modes.h
@@ -823,6 +823,7 @@ Expr * find_namespace_func(Expr * sdef, Str * method);
 Bool try_ufcs_rewrite(TypeScope * scope, Expr * e, Expr * fa, Expr * obj, Str * method, Str * type_name);
 Bool infer_func_ptr_field_call(TypeScope * scope, Expr * e, Expr * fa, Expr * obj, Expr * sdef, Str * method, I32 in_func);
 void validate_fcall_own_args(TypeScope * scope, Expr * e, TypeBinding * callee_bind);
+void resolve_fcall_return_type(TypeScope * scope, Expr * e, Str * name, TypeBinding * callee_bind, I32 in_func);
 void infer_fcall_expr(TypeScope * scope, Expr * e, I32 in_func);
 void infer_expr(TypeScope * scope, Expr * expr, I32 in_func);
 void infer_ident_expr(TypeScope * scope, Expr * expr);
