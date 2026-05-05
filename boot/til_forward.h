4637cdd59d047b78fb4fd0c7c5bfef744df71392
Jorge Timón <jtimon@jtimon.cc>
2026-05-05 22:18:45 +0100

    Issue #173 step 3e: switch Set.new to TilType

--- boot/til_forward.h
+++ boot/til_forward.h
@@ -975,7 +975,7 @@ File * File_clone(File * self);
 void File_delete(File * self, Bool * call_free);
 U32 File_size(void);
 void swap(void * a, void * b, U64 size);
-Set * Set_new(Str * elem_type);
+Set * Set_new(TilType * T);
 Set * Set_new_newi(TilType * T);
 U32 * Set_len(Set * self);
 Bool Set_has(Set * self, void * val);
