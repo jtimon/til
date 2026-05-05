8f0ec0c9df0a769ac90b6349a137df42eadccdaa
Jorge Timón <jtimon@jtimon.cc>
2026-05-05 19:50:12 +0100

    Issue #173 prep: split Map.new interfaces
    Keep Map.new on the old four-argument interface, add Map.new_newi, and migrate current source callsites plus map-literal desugaring to the new two-argument form. Verified with a clean local make clean && make test run.

--- boot/til_forward.h
+++ boot/til_forward.h
@@ -866,6 +866,7 @@ void Array_delete(Array * self, Bool * call_free);
 Array * Array_clone(Array * self);
 U32 Array_size(void);
 Map * Map_new(Str * key_type, U32 * _key_size, Str * val_type, U32 * _val_size);
+Map * Map_new_newi(Str * key_type, Str * val_type);
 U32 * Map_len(Map * self);
 void * Map_key_ptr(Map * self, U32 * i);
 void * Map_val_ptr(Map * self, U32 * i);
