632abe8c3c3714461fac692c50a7ce4a7d787ed7
Jorge Timón <jtimon@jtimon.cc>
2026-07-28 01:28:08 +0100

    Doc: Split issue #328 into implementation checkpoints

--- boot/til.c
+++ boot/til.c
@@ -12631,7 +12631,7 @@ static Str _til_str_lits[5169] = {
     (Str){.c_str = (void *)"./src/self/binder.til:1819:27", .count = 29ULL, .cap = TIL_CAP_LIT},
     (Str){.c_str = (void *)"mode lib\n\n", .count = 10ULL, .cap = TIL_CAP_LIT},
     (Str){.c_str = (void *)"0.0.1-", .count = 6ULL, .cap = TIL_CAP_LIT},
-    (Str){.c_str = (void *)"0.0.1-7f3e2c3c1", .count = 15ULL, .cap = TIL_CAP_LIT},
+    (Str){.c_str = (void *)"0.0.1-6a94e9985", .count = 15ULL, .cap = TIL_CAP_LIT},
     (Str){.c_str = (void *)"til[", .count = 4ULL, .cap = TIL_CAP_LIT},
     (Str){.c_str = (void *)"]... ", .count = 5ULL, .cap = TIL_CAP_LIT},
     (Str){.c_str = (void *)"]> ", .count = 3ULL, .cap = TIL_CAP_LIT},
@@ -277187,7 +277187,7 @@ int main(int argc, char **argv) {
     priv___src_self_interpreter_til__word_ring_idx = 0;
     { Vec__DynPtrBox *_hp = (Vec__DynPtrBox *)Vec__DynPtrBox_new(); priv___src_self_interpreter_til__scope_pool = *_hp; free(_hp); }
     BASE_VERSION = (Str){.c_str=(void*)"0.0.1-", .count=6ULL, .cap=TIL_CAP_LIT};
-    VERSION = (Str){.c_str=(void*)"0.0.1-7f3e2c3c1", .count=15ULL, .cap=TIL_CAP_LIT};
+    VERSION = (Str){.c_str=(void*)"0.0.1-6a94e9985", .count=15ULL, .cap=TIL_CAP_LIT};
     int _va_argc = argc - 1;
     USize _va_cap = _va_argc;
     Array__Str * args = malloc(sizeof(Array__Str));
