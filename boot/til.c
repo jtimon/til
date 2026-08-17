12b5f97a6a2631756d5fc514a11e1b77ba324b7f
Jorge Timón <jtimon@jtimon.cc>
2026-08-17 13:17:15 +0100

    Doc: make doc

--- boot/til.c
+++ boot/til.c
@@ -242011,7 +242011,7 @@ int main(int argc, char **argv) {
     REPL_BYTE_DEL = 127;
     REPL_HISTORY_MAX = 1000;
     REPL_ESC_TIMEOUT_MS = 50;
-    VERSION = (Str){.c_str=(void*)"0.0.1-a65fa5ef2", .count=15ULL, .cap=TIL_CAP_LIT};
+    VERSION = (Str){.c_str=(void*)"0.0.1-c5918d787", .count=15ULL, .cap=TIL_CAP_LIT};
     int _va_argc = argc - 1;
     USize _va_cap = _va_argc;
     Array__Str * args = malloc(TIL_BOX(Array__Str));
