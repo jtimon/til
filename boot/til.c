a6c1d1719cf28d1989085be4d28a690fbdef045b
Jorge Timón <jtimon@jtimon.cc>
2026-08-18 22:39:45 +0100

    Doc: Detail JVM backend plan

--- boot/til.c
+++ boot/til.c
@@ -238194,7 +238194,7 @@ int main(int argc, char **argv) {
     REPL_BYTE_DEL = 127;
     REPL_HISTORY_MAX = 1000;
     REPL_ESC_TIMEOUT_MS = 50;
-    VERSION = (Str){.c_str=(void*)"0.0.1-9dc34b3fc", .count=15ULL, .cap=TIL_CAP_LIT};
+    VERSION = (Str){.c_str=(void*)"0.0.1-039a15f59", .count=15ULL, .cap=TIL_CAP_LIT};
     int _va_argc = argc - 1;
     USize _va_cap = _va_argc;
     Array__Str _va_main_args = {0};
