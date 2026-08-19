ac4a15d8c18309dcfc190ace305ede7f8a43c7e1
Jorge Timón <jtimon@jtimon.cc>
2026-08-19 03:21:18 +0100

    Doc: Re-analyze compile-time value folding (#70)

--- boot/til.c
+++ boot/til.c
@@ -246938,7 +246938,7 @@ int main(int argc, char **argv) {
     REPL_BYTE_DEL = 127;
     REPL_HISTORY_MAX = 1000;
     REPL_ESC_TIMEOUT_MS = 50;
-    VERSION = (Str){.c_str=(void*)"0.0.1-f67c082e5", .count=15ULL, .cap=TIL_CAP_LIT};
+    VERSION = (Str){.c_str=(void*)"0.0.1-0c4f53975", .count=15ULL, .cap=TIL_CAP_LIT};
     int _va_argc = argc - 1;
     USize _va_cap = _va_argc;
     Array__Str _va_main_args = {0};
