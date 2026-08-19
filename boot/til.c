e21c624ad4aeac7dc4e3009a3613992d5afa2c6c
Jorge Timón <jtimon@jtimon.cc>
2026-08-19 01:02:43 +0100

    Doc: Cancel obsolete Vec.move_from cleanup (#207)

--- boot/til.c
+++ boot/til.c
@@ -239563,7 +239563,7 @@ int main(int argc, char **argv) {
     REPL_BYTE_DEL = 127;
     REPL_HISTORY_MAX = 1000;
     REPL_ESC_TIMEOUT_MS = 50;
-    VERSION = (Str){.c_str=(void*)"0.0.1-2cabcf60e", .count=15ULL, .cap=TIL_CAP_LIT};
+    VERSION = (Str){.c_str=(void*)"0.0.1-1783d692a", .count=15ULL, .cap=TIL_CAP_LIT};
     int _va_argc = argc - 1;
     USize _va_cap = _va_argc;
     Array__Str _va_main_args = {0};
