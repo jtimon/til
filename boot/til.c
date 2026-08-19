d7ab3d31cbc9bf35431cc65f8cd8d410b5248a88
Jorge Timón <jtimon@jtimon.cc>
2026-08-19 02:28:10 +0100

    Polish shootil browser UX (#376)

--- boot/til.c
+++ boot/til.c
@@ -240377,7 +240377,7 @@ int main(int argc, char **argv) {
     REPL_BYTE_DEL = 127;
     REPL_HISTORY_MAX = 1000;
     REPL_ESC_TIMEOUT_MS = 50;
-    VERSION = (Str){.c_str=(void*)"0.0.1-2a8ad05fe", .count=15ULL, .cap=TIL_CAP_LIT};
+    VERSION = (Str){.c_str=(void*)"0.0.1-0911663be", .count=15ULL, .cap=TIL_CAP_LIT};
     int _va_argc = argc - 1;
     USize _va_cap = _va_argc;
     Array__Str _va_main_args = {0};
