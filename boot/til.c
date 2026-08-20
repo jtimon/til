f158d90270ec3fce97652606b4b2aabf6eecaafa
Jorge Timón <jtimon@jtimon.cc>
2026-08-20 03:04:14 +0100

    Consolidate typer diagnostic count runs

--- boot/til.c
+++ boot/til.c
@@ -11177,7 +11177,7 @@ static I64 REPL_BYTE_ESC = 27;
 static I64 REPL_BYTE_DEL = 127;
 static USize REPL_HISTORY_MAX = 1000;
 static I64 REPL_ESC_TIMEOUT_MS = 50;
-static Str VERSION = {.c_str = (I8 *)"0.0.1-7a1b5fd9c", .count = 15ULL, .cap = TIL_CAP_LIT};
+static Str VERSION = {.c_str = (I8 *)"0.0.1-c7c4e23d9", .count = 15ULL, .cap = TIL_CAP_LIT};
 U8 U8_MAX = 255;
 U16 U16_MAX = 65535;
 U32 U32_MAX = 4294967295;
