ddbca5bdce4f692b1d82ade5bd2e7aa3777b3d4f
Jorge Timón <jtimon@jtimon.cc>
2026-08-20 19:16:40 +0100

    Test tutorial programs and documentation links

--- boot/til.c
+++ boot/til.c
@@ -12008,7 +12008,7 @@ static I64 REPL_BYTE_ESC = 27;
 static I64 REPL_BYTE_DEL = 127;
 static USize REPL_HISTORY_MAX = 1000;
 static I64 REPL_ESC_TIMEOUT_MS = 50;
-static Str VERSION = {.c_str = (I8 *)"0.0.1-9187ffe1c", .count = 15ULL, .cap = TIL_CAP_LIT};
+static Str VERSION = {.c_str = (I8 *)"0.0.1-39603ac81", .count = 15ULL, .cap = TIL_CAP_LIT};
 U8 U8_MAX = 255;
 U16 U16_MAX = 65535;
 U32 U32_MAX = 4294967295;
