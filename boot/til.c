00079eca8509f6f36b87165b47048867743a2d4d
Jorge Timón <jtimon@jtimon.cc>
2026-08-26 23:46:06 +0100

    Make plot axes readable at benchmark scales (#401 follow-up)

--- boot/til.c
+++ boot/til.c
@@ -12334,7 +12334,7 @@ static I64 REPL_BYTE_ESC = 27;
 static I64 REPL_BYTE_DEL = 127;
 static USize REPL_HISTORY_MAX = 1000;
 static I64 REPL_ESC_TIMEOUT_MS = 50;
-static Str VERSION = {.c_str = (I8 *)"0.0.1-b6855e790", .count = 15ULL, .cap = TIL_CAP_LIT};
+static Str VERSION = {.c_str = (I8 *)"0.0.1-7dbc7edb7", .count = 15ULL, .cap = TIL_CAP_LIT};
 U8 U8_MAX = 255;
 U16 U16_MAX = 65535;
 U32 U32_MAX = 4294967295;
